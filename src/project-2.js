a016bda21f34c8419a6e0083bb610b6e2ce5b2bc
lightyaer <mmnyed@gmail.com>
2019-03-08 17:04:29 +0530

    FIX: solution for all problems

--- src/project-2.js
+++ src/project-2.js
@@ -1,109 +1,76 @@
 // Do not change any of the function names
 
-const getBiggest = (x, y) => {
-  // x and y are integers.  Return the larger integer
-  // if they are the same return either one
+const getBiggest = (x, y) => (x >= y ? x : y);
+
+const greeting = language => {
+  switch (language) {
+    case "German":
+      return "Guten Tag!";
+    case "Spanish":
+      return "Hola!";
+    case "Chinese":
+      return "Ni Hao!";
+    default:
+      return "Hello!";
+  }
 };
 
-const greeting = (language) => {
-  // return a greeting for three different languages:
-  // language: 'German' -> 'Guten Tag!'
-  // language: 'Spanish' -> 'Hola!'
-  // language: 'Chinese' -> 'Ni Hao!'
-  // if language is undefined return 'Hello!'
+const isTenOrFive = num => num === 10 || num === 5;
+
+const isInRange = num => num < 50 && num > 20;
+
+const isInteger = num => Math.floor(num) === num;
+
+const fizzBuzz = num => {
+  if (num % 3 === 0 && num % 5 === 0) {
+    return "fizzbuzz";
+  } else if (num % 5 === 0) {
+    return "buzz";
+  } else if (num % 3 === 0) {
+    return "fizz";
+  } else {
+    return num;
+  }
 };
 
-const isTenOrFive = (num) => {
-  // return true if num is 10 or 5
-  // otherwise return false
+const isPrime = num => {
+  for (let i = 2; i <= num / 2; i++) {
+    if (num % i === 0) {
+      return false;
+    }
+  }
+  return num > 1;
 };
 
-const isInRange = (num) => {
-  // return true if num is less than 50 and greater than 20
-};
+const returnFirst = arr => arr.shift();
 
-const isInteger = (num) => {
-  // return true if num is an integer
-  // 0.8 -> false
-  // 1 -> true
-  // -10 -> true
-  // otherwise return false
-  // hint: you can solve this using Math.floor
-};
+const returnLast = arr => arr.pop();
 
-const fizzBuzz = (num) => {
-  // if num is divisible by 3 return 'fizz'
-  // if num is divisible by 5 return 'buzz'
-  // if num is divisible by 3 & 5 return 'fizzbuzz'
-  // otherwise return num
-};
+const getArrayLength = arr => arr.length;
 
-const isPrime = (num) => {
-  // return true if num is prime.
-  // otherwise return false
-  // hint: a prime number is only evenly divisible by itself and 1
-  // hint2: you can solve this using a for loop
-  // note: 0 and 1 are NOT considered prime numbers
-};
-
-const returnFirst = (arr) => {
-  // return the first item from the array
-};
-
-const returnLast = (arr) => {
-  // return the last item of the array
-};
-
-const getArrayLength = (arr) => {
-  // return the length of the array
-};
-
-const incrementByOne = (arr) => {
-  // arr is an array of integers
-  // increase each integer by one
-  // return the array
-};
+const incrementByOne = arr => arr.map(num => (num += 1));
 
 const addItemToArray = (arr, item) => {
-  // add the item to the end of the array
-  // return the array
+  arr.push(item);
+  return arr;
 };
 
 const addItemToFront = (arr, item) => {
-  // add the item to the front of the array
-  // return the array
-  // hint: use the array method .unshift
+  arr.unshift(item);
+  return arr;
 };
 
-const wordsToSentence = (words) => {
-  // words is an array of strings
-  // return a string that is all of the words concatenated together
-  // spaces need to be between each word
-  // example: ['Hello', 'world!'] -> 'Hello world!'
-};
+const wordsToSentence = words => words.join(" ");
 
-const contains = (arr, item) => {
-  // check to see if item is inside of arr
-  // return true if it is, otherwise return false
-};
+const contains = (arr, item) => arr.includes(item);
 
-const addNumbers = (numbers) => {
-  // numbers is an array of integers.
-  // add all of the integers and return the value
-};
+const addNumbers = numbers => numbers.reduce((acc, num) => acc + num);
 
-const averageTestScore = (testScores) => {
-  // testScores is an array.  Iterate over testScores and compute the average.
-  // return the average
-};
+const averageTestScore = testScores =>
+  testScores.reduce((acc, score) => acc + score) / testScores.length;
 
-const largestNumber = (numbers) => {
-  // numbers is an array of integers
-  // return the largest integer
-};
-
-// Do not modify code below this line.
-// --------------------------------
+const largestNumber = numbers =>
+  numbers.reduce((acc, num) => Math.max(acc, num));
 
 module.exports = {
   getBiggest,
--- src/project-3.js
+++ src/project-3.js
@@ -1,75 +1,69 @@
 // Do not change any of the function names
 
-const makeCat = (name, age) => {
-  // create a new object with a name property with the value set to the name argument
-  // add an age property to the object with the value set to the age argument
-  // add a method called meow that returns the string 'Meow!'
-  // return the object
-};
+const makeCat = (name, age) => ({
+  name,
+  age,
+  meow: () => "Meow!"
+});
 
 const addProperty = (object, property) => {
+  object[property] = null;
+  return object;
   // add the property to the object with a value of null
   // return the object
   // note: the property name is NOT 'property'.  The name is the value of the argument called property (a string)
 };
 
 const invokeMethod = (object, method) => {
+  object[method]();
   // method is a string that contains the name of a method on the object
   // invoke this method
   // nothing needs to be returned
 };
 
-const multiplyMysteryNumberByFive = (mysteryNumberObject) => {
+const multiplyMysteryNumberByFive = mysteryNumberObject => {
+  mysteryNumberObject["mysteryNumber"] *= 5;
+  return mysteryNumberObject["mysteryNumber"];
   // mysteryNumberObject has a property called mysteryNumber
   // multiply the mysteryNumber property by 5 and return the product
 };
 
 const deleteProperty = (object, property) => {
+  delete object[property];
+  return object;
   // remove the property from the object
   // return the object
 };
 
-const newUser = (name, email, password) => {
-  // create a new object with properties matching the arguments passed in.
-  // return the new object
-};
+const newUser = (name, email, password) => ({
+  name,
+  email,
+  password
+});
 
-const hasEmail = (user) => {
-  // return true if the user has a value for the property 'email'
-  // otherwise return false
-};
+const hasEmail = user => user.hasOwnProperty("email") && user.email !== "";
 
-const hasProperty = (object, property) => {
-  // return true if the object has the value of the property argument
-  // property is a string
-  // otherwise return false
-};
+const hasProperty = (object, property) => object.hasOwnProperty(property);
 
-const verifyPassword = (user, password) => {
-  // check to see if the provided password matches the password property on the user object
-  // return true if they match
-  // otherwise return false
-};
+const verifyPassword = (user, password) => user.password === password;
 
 const updatePassword = (user, newPassword) => {
-  // replace the existing password on the user object with the value of newPassword
-  // return the object
+  user["password"] = newPassword;
+  return user;
 };
 
 const addFriend = (user, newFriend) => {
-  // user has a property called friends that is an array
-  // add newFriend to the end of the friends array
-  // return the user object
+  user["friends"].push(newFriend);
+  return user;
 };
 
-const setUsersToPremium = (users) => {
-  // users is an array of user objects.
-  // each user object has the property 'isPremium'
-  // set each user's isPremium property to true
-  // return the users array
+const setUsersToPremium = users => {
+  users.map(user => (user.isPremium = true));
+  return users;
 };
 
-const sumUserPostLikes = (user) => {
+const sumUserPostLikes = user => {
+  return user["posts"].map(post => post.likes).reduce((acc, val) => acc + val);
   // user has an array property called 'posts'
   // posts is an array of post objects
   // each post object has an integer property called 'likes'
@@ -77,7 +71,12 @@ const sumUserPostLikes = (user) => {
   // return the sum
 };
 
-const addCalculateDiscountPriceMethod = (storeItem) => {
+const addCalculateDiscountPriceMethod = storeItem => {
+  storeItem["calculateDiscountPrice"] = function() {
+    return this.price - this.price * this.discountPercentage;
+  };
+
+  return storeItem;
   // Add a method to the storeItem object called 'calculateDiscountPrice'
   // This method should multiply the storeItem's 'price' and 'discountPercentage' to get the discount
   // The method then subtracts the discount from the price and returns the discounted price
