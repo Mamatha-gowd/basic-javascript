08efd086ed3affad59aa61e410d5e56bdf25db50
sourobrata.sarkar <sourobrata.sarkar@mountblue.io>
2019-03-11 17:48:36 +0530

    FEAT: Solution of problems

--- src/project-2.js
+++ src/project-2.js
@@ -1,9 +1,6 @@
 // Do not change any of the function names
 
-const getBiggest = (x, y) => {
-  // x and y are integers.  Return the larger integer
-  // if they are the same return either one
-};
+const getBiggest = (x, y) => x > y ? x : y
 
 const greeting = (language) => {
   // return a greeting for three different languages:
@@ -11,32 +8,30 @@ const greeting = (language) => {
   // language: 'Spanish' -> 'Hola!'
   // language: 'Chinese' -> 'Ni Hao!'
   // if language is undefined return 'Hello!'
+  if (language === 'German') return 'Guten Tag!'
+  if (language === 'Spanish') return 'Hola!'
+  if (language === 'Chinese') return 'Ni Hao!'
+  else return 'Hello!'
 };
 
-const isTenOrFive = (num) => {
-  // return true if num is 10 or 5
-  // otherwise return false
-};
+const isTenOrFive = (num) => num === 10 || num === 5
 
-const isInRange = (num) => {
-  // return true if num is less than 50 and greater than 20
-};
+const isInRange = (num) => num > 20 && num < 50
 
-const isInteger = (num) => {
-  // return true if num is an integer
-  // 0.8 -> false
-  // 1 -> true
-  // -10 -> true
-  // otherwise return false
-  // hint: you can solve this using Math.floor
-};
+const isInteger = (num) => (Math.floor(num) - num === 0)
+
 
 const fizzBuzz = (num) => {
   // if num is divisible by 3 return 'fizz'
   // if num is divisible by 5 return 'buzz'
   // if num is divisible by 3 & 5 return 'fizzbuzz'
   // otherwise return num
+  if (num % 3 === 0 && num % 5 === 0) return 'fizzbuzz'
+  else if (num % 3 === 0) return 'fizz'
+  else if (num % 5 === 0) return 'buzz'
+  else return num
 };
+
 
 const isPrime = (num) => {
   // return true if num is prime.
@@ -44,63 +39,56 @@ const isPrime = (num) => {
   // hint: a prime number is only evenly divisible by itself and 1
   // hint2: you can solve this using a for loop
   // note: 0 and 1 are NOT considered prime numbers
+  if (num === 0 || num === 1) return false
+  for (let i = 2; i < num / 2; i++) {
+    if (num % i === 0) return false;
+  }
+  return true;
 };
 
-const returnFirst = (arr) => {
-  // return the first item from the array
-};
 
-const returnLast = (arr) => {
-  // return the last item of the array
-};
+const returnFirst = (arr) => arr[0]
 
-const getArrayLength = (arr) => {
-  // return the length of the array
-};
+const returnLast = (arr) => arr[arr.length - 1]
 
-const incrementByOne = (arr) => {
-  // arr is an array of integers
-  // increase each integer by one
-  // return the array
-};
+
+
+const getArrayLength = (arr) => arr.length
+
+const incrementByOne = (arr) => arr.map((elem) => elem + 1)
+
 
 const addItemToArray = (arr, item) => {
   // add the item to the end of the array
   // return the array
+  arr.push(item)
+  return arr
 };
+
 
 const addItemToFront = (arr, item) => {
   // add the item to the front of the array
   // return the array
   // hint: use the array method .unshift
+  arr.unshift(item)
+  return arr
 };
 
-const wordsToSentence = (words) => {
-  // words is an array of strings
-  // return a string that is all of the words concatenated together
-  // spaces need to be between each word
-  // example: ['Hello', 'world!'] -> 'Hello world!'
-};
+const wordsToSentence = (words) => words.join(' ');
 
-const contains = (arr, item) => {
-  // check to see if item is inside of arr
-  // return true if it is, otherwise return false
-};
+const contains = (arr, item) => arr.includes(item)
 
-const addNumbers = (numbers) => {
-  // numbers is an array of integers.
-  // add all of the integers and return the value
-};
 
-const averageTestScore = (testScores) => {
-  // testScores is an array.  Iterate over testScores and compute the average.
-  // return the average
-};
+const addNumbers = (numbers) => numbers.reduce((sum, num) => sum + num)
 
-const largestNumber = (numbers) => {
-  // numbers is an array of integers
-  // return the largest integer
-};
+
+const averageTestScore = (testScores) => testScores.reduce((sum, score) => sum + score) / testScores.length
+
+
+
+const largestNumber = (numbers) => Math.max(...numbers)
+
+
 
 // Do not modify code below this line.
 // --------------------------------
