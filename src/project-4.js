cea307881de5cfa8559c158780a4ab403ec1a0f2
Divya Prakash <kmr.paras@gmail.com>
2019-03-12 12:55:06 +0530

    add solution for all exercises and problems of basic-javascript assignment

--- src/project-4.js
+++ src/project-4.js
@@ -1,31 +1,47 @@
 const getFirstItem = (collection, cb) => {
   // invoke the callback function and pass the first item from the collection in as an argument
+  cb(collection[0]);
 };
 
 const getLength = (collection, cb) => {
   // Write a function called getLength that passes the length of the array into the callback
+  cb(collection.length);
 };
 
 const getLastItem = (collection, cb) => {
   // Write a function called getLastItem which passes the getLastItem item of the array into the callback
+  cb(collection[collection.length-1]);
 };
 
 const sumNums = (x, y, cb) => {
   // Write a function called sumNums that adds two numbers and passes the result to the callback
+  let sum=x+y;
+  cb(sum);
 };
 
 const multiplyNums = (x, y, cb) => {
   // Write a function called multiplyNums that multiplies two numbers and passes the result to the callback
+  let mul=x*y;
+  cb(mul);
 };
 
 const contains = (collection, item, cb) => {
   // Write a function called contains that checks if an item is present inside of the given array.
   // Pass true to the callback if it is, otherwise pass false
+  let check=collection.includes(item);
+  cb(check);
 };
 
 const removeDuplicates = (collection, cb) => {
   // Write a function called removeDuplicates that removes all duplicate values from the given array.
   // Pass the array to the callback function.  Do not mutate the original array.
+  let arr=[];
+  for(let i=0;i<collection.length;i++){
+    if(arr.indexOf(collection[i]) == -1){
+      arr.push(collection[i]);
+    }
+  }
+  cb(arr);
 };
 
 module.exports = {
