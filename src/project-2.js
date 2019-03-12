4ba9b3a3ebc320e8a84872bbd405497d72fa1410
Omkar Chari <omkar.chari@mountblue.io>
2019-03-12 05:59:53 +0530

    Basic Javascript assignment solution is added to the projects

--- src/project-2.js
+++ src/project-2.js
@@ -3,6 +3,7 @@
 const getBiggest = (x, y) => {
   // x and y are integers.  Return the larger integer
   // if they are the same return either one
+  return x > y ? x : y;
 };
 
 const greeting = (language) => {
@@ -11,15 +12,27 @@ const greeting = (language) => {
   // language: 'Spanish' -> 'Hola!'
   // language: 'Chinese' -> 'Ni Hao!'
   // if language is undefined return 'Hello!'
+  if (language === 'German') {
+    return 'Guten Tag!';
+  }
+  if (language === 'Spanish') {
+    return 'Hola!';
+  }
+  if (language === 'Chinese') {
+    return 'Ni Hao!';
+  }
+  return 'Hello!';
 };
 
 const isTenOrFive = (num) => {
   // return true if num is 10 or 5
   // otherwise return false
+  return num === 10 || num === 5;
 };
 
 const isInRange = (num) => {
   // return true if num is less than 50 and greater than 20
+  return num < 50 && num > 20;
 };
 
 const isInteger = (num) => {
@@ -29,6 +42,7 @@ const isInteger = (num) => {
   // -10 -> true
   // otherwise return false
   // hint: you can solve this using Math.floor
+  return ((num - Math.floor(num)) === 0);
 };
 
 const fizzBuzz = (num) => {
@@ -36,6 +50,14 @@ const fizzBuzz = (num) => {
   // if num is divisible by 5 return 'buzz'
   // if num is divisible by 3 & 5 return 'fizzbuzz'
   // otherwise return num
+  if (num % 15 === 0) {
+    return 'fizzbuzz';
+  } else if (num % 5 === 0) {
+    return 'buzz';
+  } else if (num % 3 === 0) {
+    return 'fizz';
+  }
+  return num;
 };
 
 const isPrime = (num) => {
@@ -44,35 +66,60 @@ const isPrime = (num) => {
   // hint: a prime number is only evenly divisible by itself and 1
   // hint2: you can solve this using a for loop
   // note: 0 and 1 are NOT considered prime numbers
+  if (num === 2) {
+    return true;
+  } else if (num > 1) {
+    for (var i = 2; i < num; i++) {
+
+      if (num % i !== 0) {
+        return true;
+      } else if (num === i * i) {
+        return false
+      } else {
+        return false;
+      }
+    }
+  } else {
+    return false;
+  }
 };
 
 const returnFirst = (arr) => {
   // return the first item from the array
+  return (arr[0]);
 };
 
 const returnLast = (arr) => {
   // return the last item of the array
+  return arr.pop();
 };
 
 const getArrayLength = (arr) => {
   // return the length of the array
+  return arr.length;
 };
 
 const incrementByOne = (arr) => {
   // arr is an array of integers
   // increase each integer by one
   // return the array
+  return arr.map((item) => {
+    return item + 1;
+  });
 };
 
 const addItemToArray = (arr, item) => {
   // add the item to the end of the array
   // return the array
+  return arr.concat(item);
 };
 
 const addItemToFront = (arr, item) => {
   // add the item to the front of the array
   // return the array
   // hint: use the array method .unshift
+  arr.unshift(item);
+  return arr;
 };
 
 const wordsToSentence = (words) => {
@@ -80,26 +127,37 @@ const wordsToSentence = (words) => {
   // return a string that is all of the words concatenated together
   // spaces need to be between each word
   // example: ['Hello', 'world!'] -> 'Hello world!'
+  return (words.join(' '));
 };
 
 const contains = (arr, item) => {
   // check to see if item is inside of arr
   // return true if it is, otherwise return false
+  return arr.includes(item);
 };
 
 const addNumbers = (numbers) => {
   // numbers is an array of integers.
   // add all of the integers and return the value
+  return numbers.reduce((first, second) => {
+    return first += second;
+  });
 };
 
 const averageTestScore = (testScores) => {
   // testScores is an array.  Iterate over testScores and compute the average.
   // return the average
+  return (testScores.reduce((first, second) => {
+    return first += second;
+  }) / testScores.length);
 };
 
 const largestNumber = (numbers) => {
   // numbers is an array of integers
   // return the largest integer
+  return (numbers.reduce((first, second) => {
+    return first > second ? first : second;
+  }));
 };
 
 // Do not modify code below this line.
@@ -124,4 +182,5 @@ module.exports = {
   addNumbers,
   averageTestScore,
   largestNumber
+  // eslint-disable-next-line eol-last
 };
